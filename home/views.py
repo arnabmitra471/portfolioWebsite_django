39c3a40f6cd6ab1dd806d40cc6c71a704c9c91c9
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-23 18:59:33 +0530

    Laying the pipeline of the portfolio website in django

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("This is my home page")
+
+def about(request):
+    return HttpResponse("This is my about page")
+
+def contact(request):
+    return HttpResponse("This is my contact page")
+
+def projects(request):
+    return HttpResponse("This is my prjects page")
+
